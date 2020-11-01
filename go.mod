4594edaa0257fa4d33292bcb71d1a20c43cdbf60
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-11-01 14:37:26 +0700

    feat: implement viper config file

--- go.mod
+++ go.mod
@@ -8,8 +8,7 @@ require (
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/lib/pq v1.8.0
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
 	github.com/ugorji/go v1.1.13 // indirect
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 
 	"github.com/milhamhidayat/simplebank/api"
 	db "github.com/milhamhidayat/simplebank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://simplebank:simplebank-pass@localhost:5432/simplebank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/milhamhidayat/simplebank/db/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
